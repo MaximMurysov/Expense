7789d90ded8722909696588a1b2c74efab7bd9b4
Maxim Murysov <murisov98@mail.ru>
2026-02-11 10:45:24 +0300

    feat: add income/expense states and history structure

--- src/components/ExpenseTracker.jsx
+++ src/components/ExpenseTracker.jsx
@@ -3,13 +3,31 @@ import styles from "./styles.module.css";
 
 function ExpenseTracker() {
   const [totalSum, setTotalSum] = useState(0);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [history, setHistory] = useState([]);
+
   return (
     <div className={styles.expense}>
       <div className={styles["expense-container"]}>
         <h2 className={styles["expense-title"]}>Expense tracker</h2>
-        <div className={styles["expense-balance"]}>
+        <div className={styles["expense-total__balance"]}>
           <h3>Your balance</h3>
           <p>$ {totalSum}</p>
+        </div>
+        <div className={styles["income-expense"]}>
+          <div className={styles.income}>
+            <h4>INCOME</h4>
+            <p>$ {income}</p>
+          </div>
+          <div className={styles["expense-balance"]}>
+            <h4>EXPENSE</h4>
+            <p>$ {expense}</p>
+          </div>
+        </div>
+        <div className={styles.history}>
+          <h4>HISTORY</h4>
+          {history.map((elem) => ({ elem }))}
         </div>
       </div>
     </div>
